0e760586568cbcdb5da349056448ae9315a6cb90
DANNOSIUCUTE <paody2004@gmail.com>
2026-03-30 18:04:37 +0700

    check

--- RemoteControlSystem/AgentApp/obj/Debug/net9.0-windows/AgentApp.AssemblyInfo.cs
+++ RemoteControlSystem/AgentApp/obj/Debug/net9.0-windows/AgentApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95183605878efc03ebdcee0110b1301b979371f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d91d9e532d6da8d5dc0d00cc77146dc8b659ce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RemoteControlSystem/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ RemoteControlSystem/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da558d209493e743cb5e207a931e871b845229f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d91d9e532d6da8d5dc0d00cc77146dc8b659ce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
