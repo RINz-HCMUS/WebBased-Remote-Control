bfc084dc7e80b217aca1e308049bc8f1bb8c9dc9
DANNOSIUCUTE <paody2004@gmail.com>
2026-03-30 18:31:26 +0700

    ok

--- RemoteControlSystem/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ RemoteControlSystem/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09943b948a072b2aea38320dc1c2a8f469cd5264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81349211a0bee1f44a27759c838f251ce56ad5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
