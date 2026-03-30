98d2625555e8c95901383356e30860ca7e3c1d41
RIN <tuanv2322004@gmail.com>
2026-03-30 18:45:51 +0700

    demo v3

--- RemoteControlSystem/AgentApp/obj/Debug/net9.0-windows/AgentApp.AssemblyInfo.cs
+++ RemoteControlSystem/AgentApp/obj/Debug/net9.0-windows/AgentApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09943b948a072b2aea38320dc1c2a8f469cd5264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81349211a0bee1f44a27759c838f251ce56ad5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
